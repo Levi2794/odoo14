e42d76fdc555d4e2fc579ae8a06aa0e8c60814c0
Levi2794 <traceur.rick@gmail.com>
2021-05-19 16:08:48 -0500

    update library

---
+++ library/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Management",
+
+    'summary': """
+        Library Management
+        """,
+
+    'author': "OEC",
+    'website': "http://www.odooerpcloud.com",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    'installable': True,
+
+}
